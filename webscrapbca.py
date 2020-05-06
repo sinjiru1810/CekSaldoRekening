2caa2b61bea7ab6d4861a4068b0766ff4dd6230e
sinjiru1810 <56338495+sinjiru1810@users.noreply.github.com>
2020-05-07 02:13:54 +0700

    Update webscrapbca.py

--- webscrapbca.py
+++ webscrapbca.py
@@ -7,10 +7,14 @@ info_login = {
     'password': '<pin klik bca anda>'
 }
 
+
 #xpath html yang sudah di inspek
 xpath = {
-    'balance'   : '/html/body/table[3]/tbody/tr[2]/td[4]/div/font', #xpath value saldo
     'info_saldo': '//table[1]/tbody/tr[1]/td[2]/table[1]/tbody/tr[4]/td[1]/table[1]/tbody/tr[1]/td[2]/font/a', #xpath menu bar kiri
+    'rekening'  : '/html/body/table[3]/tbody/tr[2]/td[1]/div/font',
+    'jenis'     : '/html/body/table[3]/tbody/tr[2]/td[2]/div/font',
+    'matauang'  : '/html/body/table[3]/tbody/tr[2]/td[3]/div/font',
+    'saldo'     : '/html/body/table[3]/tbody/tr[2]/td[4]/div/font', #xpath value saldo
 }
 
 #membuka browser
@@ -18,7 +22,7 @@ br = webdriver.Chrome('/home/sinjirusetyawan/Downloads/chromedriver')
 
 #buka web, tunggu 1 detik agar semua selesai load, lanjut
 br.get('https://ibank.klikbca.com/authentication.do?value(actions)=logout')
-time.sleep(1)
+time.sleep(0.5)
 
 #agar bisa kontrol pindah tab
 br.window_handles
@@ -44,8 +48,14 @@ br.find_element_by_tag_name('body').send_keys(Keys.CONTROL + "2")
 br.switch_to.window(br.window_handles[-1])
 
 #cari eelemen berisi nilai saldo dan dicetak
-saldo = br.find_element_by_xpath(xpath['balance'])
-print("SALDO ANDA : Rp. ",saldo.text)
+norek = br.find_element_by_xpath(xpath['rekening'])
+jenis = br.find_element_by_xpath(xpath['jenis'])
+matauang = br.find_element_by_xpath(xpath['matauang'])
+saldo = br.find_element_by_xpath(xpath['saldo'])
+print("NO REK     : ",norek.text)
+print("JENIS REK  : ",jenis.text)
+print("MATA UANG  : ",matauang.text)
+print("SALDO ANDA :  Rp. ",saldo.text)
 
 #tutup browser
 #ActionChains(br).key_down(Keys.CONTROL).key_down(Keys.SHIFT).send_keys('w').key_up(Keys.CONTROL).key_up(Keys.SHIFT).perform()
